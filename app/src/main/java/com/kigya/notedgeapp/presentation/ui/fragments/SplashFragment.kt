de0afd5fc26f17e773c4e60e3145468792a107cb
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-05-05 20:43:49 +0300

    Add splash anim logic

--- app/src/main/java/com/kigya/notedgeapp/presentation/ui/fragments/SplashFragment.kt
+++ app/src/main/java/com/kigya/notedgeapp/presentation/ui/fragments/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.kigya.notedgeapp.presentation.ui.fragments
 
+import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import com.kigya.notedgeapp.R
 import com.kigya.notedgeapp.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
@@ -23,7 +26,12 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding
+        val animR: AnimatedVectorDrawableCompat? =
+            AnimatedVectorDrawableCompat.create(view.context, R.drawable.anim_logo)
+        animR?.let {
+            binding.mainLogo.setImageDrawable(it)
+        }
+        (binding.mainLogo.drawable as Animatable?)?.start()
     }
 
     override fun onDestroyView() {
