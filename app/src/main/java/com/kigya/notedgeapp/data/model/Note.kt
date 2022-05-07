b221934a22952744c00f9a730eb6daf6394a476d
Vladimir Larichev <freeky92s@gmail.com>
2022-05-07 18:49:46 +0300

    note to data class

--- app/src/main/java/com/kigya/notedgeapp/data/model/Note.kt
+++ app/src/main/java/com/kigya/notedgeapp/data/model/Note.kt
@@ -1,24 +1,21 @@
 package com.kigya.notedgeapp.data.model
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.io.Serializable
+import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 @Entity(tableName = "notes")
-class Note : Serializable {
-
+@Parcelize
+data class Note(
     @PrimaryKey
-    var id: UUID = UUID.randomUUID()
-
+    var id: UUID = UUID.randomUUID(),
     @ColumnInfo(name = "title")
-    var title: String = ""
-
+    var title: String = "",
     @ColumnInfo(name = "date_time")
-    var dateTime: Date = Date()
-
+    var dateTime: Date = Date(),
     @ColumnInfo(name = "note_text")
-    var noteText: String = ""
-
-}
+    var noteText: String = "",
+) : Parcelable
